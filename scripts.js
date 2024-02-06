7efae8ad91149fe9adb1541b62b59cafc47f022f
voskreseniev <roman.mayber@gmail.com>
2024-02-06 13:34:00 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@
 function openSettings() {
   window.location.href = 'intent:#Intent;action=android.settings.SETTINGS;end';
 }
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY + window.innerHeight;
+    var pageHeight = document.body.scrollHeight;
+
+    // Проверяем, прокручена ли страница до конца
+    if (scrollPosition >= pageHeight) {
+        document.querySelector('footer').style.display = 'none';
+    } else {
+        document.querySelector('footer').style.display = 'block';
+    }
+});
